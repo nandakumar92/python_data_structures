958ac35c9b1556f1df4031c23405f642f1572980
nandakumar92 <nandakumar.uday92@gmail.com>
2020-12-13 18:17:36 -0800

    first commit

---
+++ single_linked list/main.py
@@ -0,0 +1,89 @@
+from node import node
+from linked_list import List
+
+
+def insert_at_head(list,value):
+
+    temp=node(value)   
+    temp.next_element=list.header
+    list.header=temp
+
+
+def insert_at_tail(list,value):
+
+    new_node=node(value)
+    
+    if list.is_empty():
+         list.header=new_node
+    
+    temp=list.get_head()
+
+    while temp.next_element:           
+           temp=temp.next_element
+  
+    temp.next_element=new_node
+
+
+def search(list,value):
+
+     if list.is_empty():
+         return False
+
+     temp=list.get_head()
+
+     while temp.next_element:
+
+         if temp.data==value:
+             return True
+
+         temp=temp.next_element
+    
+     return False
+
+
+def delete_at_head(list,value):
+
+     tmp=list.get_head()
+     list.header=tmp.next_element()
+     tmp.next_element=None
+
+
+def delete_value(list,value):
+
+    current=list.get_head()
+    prev=None
+    if current.data==value:
+        delete_at_head(list,value)
+
+    while current.next_element:
+
+        if current.data==value:
+            prev.next_element=current.next_element
+            current.next_element=None
+        
+        prev=current
+        current=current.next_element
+
+
+def get_count(list):
+
+    tmp=list.get_head()
+    i=0
+    while tmp is not None:
+        i=i+1
+        tmp=tmp.next_element
+
+
+    return i
+
+                   
+list=List()
+
+for i in range(10):
+    insert_at_tail(list,i)
+
+print(get_count(list))
+
+
+
+    
---
+++ single_linked list/node.py
@@ -0,0 +1,9 @@
+
+
+class node:
+
+    def __init__(self,data):
+        self.data=data
+        self.next_element=None
+
+    
